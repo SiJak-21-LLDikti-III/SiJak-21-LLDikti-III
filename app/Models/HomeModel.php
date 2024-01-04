f09e73e2bf341baa1bc10417cd562837eba07dd1
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-01-04 18:31:40 +0700

    Added model for tb_identitas

--- app/Models/HomeModel.php
+++ app/Models/HomeModel.php
@@ -6,15 +6,24 @@ use CodeIgniter\Model;
 
 class HomeModel extends Model
 {
-    protected $table = 'tb_sijak'; // Sesuaikan dengan nama tabel Anda
-    protected $primaryKey = 'id'; // Sesuaikan dengan primary key tabel
+    protected $table1 = 'tb_sijak';
+    protected $table2 = 'tb_identitas';
 
     public function getUserData($npwp, $birthDate, $yearOption)
     {
         $mperlan = "mperlan_H04-H05";
-        return $this->where('npwp_A1', $npwp)
+        return $this->db->table($this->table1)
+            ->where('npwp_A1', $npwp)
             ->where('tgl_lahir', $birthDate)
-            ->where('SUBSTRING(`' . $mperlan . '`, 7)', $yearOption, false)
-            ->first();
+            ->where('SUBSTRING(`' . $mperlan . '`, 1, 4)', $yearOption, false)
+            ->get()
+            ->getRow();
+    }
+
+    public function getIDPP()
+    {
+        return $this->db->table($this->table2)
+            ->get()
+            ->getResult();
     }
 }
