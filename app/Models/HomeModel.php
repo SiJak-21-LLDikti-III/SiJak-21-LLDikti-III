7e5df6349d29e1a3dc947cc286db7718cf87412f
Arizki Putra Rahman <arizkijitsu@gmail.com>
2024-01-04 11:58:37 +0700

    Changes model for yearoption

--- app/Models/HomeModel.php
+++ app/Models/HomeModel.php
@@ -9,10 +9,12 @@ class HomeModel extends Model
     protected $table = 'tb_sijak'; // Sesuaikan dengan nama tabel Anda
     protected $primaryKey = 'id'; // Sesuaikan dengan primary key tabel
 
-    public function getUserData($npwp, $birthDate)
+    public function getUserData($npwp, $birthDate, $yearOption)
     {
+        $mperlan = "mperlan_H04-H05";
         return $this->where('npwp_A1', $npwp)
-            ->where('tgl_lahir', $birthDate) // Sesuaikan nama kolom dengan tabel Anda
+            ->where('tgl_lahir', $birthDate)
+            ->where('SUBSTRING(`' . $mperlan . '`, 7)', $yearOption, false)
             ->first();
     }
 }
